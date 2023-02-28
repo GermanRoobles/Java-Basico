2c691352abf4041edc91fee8a06b61815ee9ac4f
GermanRoobles <117644675+GermanRoobles@users.noreply.github.com>
2023-02-28 12:05:32 +0100

    Add files via upload

---
+++ Ejercicio_1/MetodoMain.java
@@ -0,0 +1,32 @@
+package Ejercicio_1;
+
+public class MetodoMain {
+    public static void main(String[] agrs) {
+        // enteros
+        byte numero1 = 1;
+        short numero2 = 10;
+        int numero3 = 50;
+        long numero4 = 100;
+        System.out.println("numero enteros: " + numero1 + ", " + numero2 + ", " + numero3 + ", " + numero4);
+
+        // decimales
+        float numero5 = 4.7f;
+        double numero6 = 7.88d;
+        System.out.println("numero decimales: " + numero5 + ", " + numero6);
+
+        // cadena de texto
+        String nombre = "German";
+        String apellidos = "Robles Lopez";
+        System.out.println("nombre completo: " + nombre + " " + apellidos);
+
+        // caracter
+        char caracter = 'a';
+        System.out.println("caracter: " + caracter);
+
+        // booleanos
+        boolean verdadero = true;
+        boolean falso = false;
+        System.out.println("booleanos: " + verdadero + ", " + falso);
+
+    }
+}
