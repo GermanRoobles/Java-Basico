ebe3db55939c36219298bdf962b0bd08c8926135
GermanRoobles <117644675+GermanRoobles@users.noreply.github.com>
2023-03-03 11:00:06 +0100

    Add files via upload

---
+++ Ejercicio_3/src/ejercicio_3.java
@@ -0,0 +1,11 @@
+public class ejercicio_3 {
+    public static void main(String[] args) {
+        
+        String[] texto = new String[]{"Texto 1", "Texto 2", "Texto 3"};
+
+        for(int i = 0; i < texto.length; i++) {
+            System.out.print(texto[i]);
+        }
+
+    }
+}
